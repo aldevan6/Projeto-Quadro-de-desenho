56a634db80e356fb06474f2be24886462ac75609
Aldevan Silva <aldevan6@gmail.com>
2022-07-25 22:17:36 -0300

    Arquivos adicionados

---
+++ script.js
@@ -0,0 +1,23 @@
+//Inicial data
+let currentColor = 'black';
+
+let screen = document.querySelector("#tela");
+let ctx = screen.getContext('2d');
+
+//Events 
+
+document.querySelectorAll('.colorArea .color').forEach(item =>{
+    item.addEventListener('click', colorClickEvent);
+});
+
+
+//Funtions
+
+function colorClickEvent(e) {
+    let color = e.target.getAttribute('data-color');
+
+    currentColor = color;
+
+    document.querySelector('.color.active').classList.remove('active');
+    e.target.classList.add('active');
+}
