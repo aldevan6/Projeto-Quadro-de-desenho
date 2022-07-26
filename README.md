7e41a53ea4f014e3ea62f480717347e724da35bb
Aldevan Silva <aldevan6@gmail.com>
2022-07-26 00:14:41 -0300

    Projeto finalizado

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # Projeto-Quadro-de-desenho
+
+<h2>Conhecimentos obtidos com o projeto</h2>
+
+<ul>
+    <li>Manipulação do DOM. </li>
+    <li>Utilização da tag <b>canvas</b>. </li>
+    <li>Eventos de mouse. </li>
+    <li>Pegar posição do ponteiro do mouse na tela e compensar com base nas medidas do elemento. </li>
+</ul>
+<b> Descrição:</b> <br/>
+Este projeto possibilita que o usuário escreva algo ou faça desenhos utilizando o click e araste do mouse. <br/>
+<h2>Captura de tela do projeto:</h2>
+
+![](screen01.JPG)
--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 //Inicial data
 let currentColor = 'black';
+let canDrow = false;
+let mouseX = 0;
+let mouseY = 0;
 
 let screen = document.querySelector("#tela");
 let ctx = screen.getContext('2d');
@@ -10,14 +13,54 @@ document.querySelectorAll('.colorArea .color').forEach(item =>{
     item.addEventListener('click', colorClickEvent);
 });
 
+screen.addEventListener('mousedown', mouseDownEvent);
+screen.addEventListener('mousemove', mouseMoveEvent);
+screen.addEventListener('mouseup', mouseUpEvent);
+document.querySelector('.clear').addEventListener('click', clearScreen);
+
 
 //Funtions
-
 function colorClickEvent(e) {
     let color = e.target.getAttribute('data-color');
-
     currentColor = color;
-
     document.querySelector('.color.active').classList.remove('active');
     e.target.classList.add('active');
+}
+
+function mouseDownEvent(e){
+    canDrow =true;
+    mouseX = e.pageX - screen.offsetLeft; //Salva a posição do elemento até o fim da página para a esquerda.
+    mouseY = e.pageY - screen.offsetTop; //Salva a posição do elemento até o fim da página para cima.
+}
+
+function mouseMoveEvent(e){
+    if (canDrow) {
+        draw(e.pageX , e.pageY);
+    }
+}
+
+function mouseUpEvent() {
+    canDrow = false;
+}
+
+function draw(x ,y) {
+    let pointX = x - screen.offsetLeft;
+    let pointY = y - screen.offsetTop;
+
+    ctx.beginPath();
+    ctx.lineWidth = 5;
+    ctx.lineJoin = "round";
+    ctx.moveTo(mouseX, mouseY);
+    ctx.lineTo(pointX, pointY);
+    ctx.closePath();
+    ctx.strokeStyle = currentColor;
+    ctx.stroke();
+
+    mouseX = pointX;
+    mouseY = pointY;
+}
+
+function clearScreen() {
+    ctx.setTransform(1, 0, 0, 1, 0, 0);
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
 }
